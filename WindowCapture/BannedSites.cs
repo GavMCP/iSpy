8b1712a78b498210107a2b93c66878d0ec1dfc03
GavMCP <36470094+GavMCP@users.noreply.github.com>
2020-03-19 17:11:30 +0000

    Added DLL projects to LOG branch

---
+++ WindowCapture/BannedSites.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowCapture
+{
+    public static class BannedSites
+    {
+        public static List<string> ListBannedSites = new List<string> { "Facebook", "PornHub" };
+               
+    }
+}
