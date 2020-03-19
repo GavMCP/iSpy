0b4f1029b3962baa31a3d3b8b085a674d16efd76
GavMCP <36470094+GavMCP@users.noreply.github.com>
2020-03-19 17:12:41 +0000

    Add files via upload

---
+++ KeyCapture/Classes/Hooks/HookID.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyCapture.Classes.Hooks
+{
+    public class HookID
+    {
+        public static IntPtr _hookID { get; } = IntPtr.Zero;
+        //public static LowLevelKeyboardProc _proc = HookCallback;
+
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        
+
+    }
+    
+}
